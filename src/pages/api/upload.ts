e659255f3a2e1da9d5cabb428492f57bf48eb751
Henrique Jensen <henriquejensen@hotmail.com>
2023-07-06 17:04:26 -0300

    another try

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -27,18 +27,18 @@ const storageProduction = multers3({
   },
 });
 
-const storageDevelopment = multer.diskStorage({
-  destination: "./public/uploads/", // Set the destination folder for uploaded files
-  filename: (req: any, file, cb) => {
-    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    const fileExtension = extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${fileExtension}`);
-  }
-})
+// const storageDevelopment = multer.diskStorage({
+//   destination: "./public/uploads/", // Set the destination folder for uploaded files
+//   filename: (req: any, file, cb) => {
+//     const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+//     const fileExtension = extname(file.originalname);
+//     cb(null, `${file.fieldname}-${uniqueSuffix}${fileExtension}`);
+//   }
+// })
 
 // Configure Multer
 const upload = multer({
-  storage: process.env.AWS_BUCKET_NAME ? storageProduction : storageDevelopment,
+  storage: storageProduction,
   limits: { fileSize: 1024 * 1024 * 2 }, // 2 MB
   fileFilter: (_, file, cb) => {
     const allowedExtensions = [".png", ".jpg", ".jpeg", ".svg"];
@@ -67,11 +67,12 @@ const uploadHandler = (request: Request, res: Response) => {
         return res.status(500).json({ error: err.message });
       }
 
-      let url = `http://localhost:3000/uploads/${request.file.filename}`;
+      // let url = `http://localhost:3000/uploads/${request.file.filename}`;
+      let url = `${request.file.location}`;
 
-      if (process.env.AWS_BUCKET_NAME) {
-        url = `${request.file.location}`;
-      }
+      // if (process.env.AWS_BUCKET_NAME) {
+      //   url = `${request.file.location}`;
+      // }
 
       return res
         .status(200)
